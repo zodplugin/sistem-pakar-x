9aa1d217b10e6cc1fce3edbaa6849640e4b59159
zodplugin <zodplugin@gmail.com>
2024-04-13 02:05:28 +0700

    feat: updated

--- src/pages/ChooseDoctor/index.js
+++ src/pages/ChooseDoctor/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Header, List} from '../../components';
