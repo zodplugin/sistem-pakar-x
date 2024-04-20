9075eccc460b4017661c0b641d83ba44146f5a12
zodplugin <zodplugin@gmail.com>
2024-04-21 00:23:16 +0700

    feat: list condition

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import {Header, ChatItem, InputChat} from '../../components';
--- src/pages/Question/index.js
+++ src/pages/Question/index.js
@@ -1,40 +1,49 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Button } from 'react-native';
-import { RadioButton } from 'react-native-paper'; 
-import { useNavigation } from '@react-navigation/native';
+/* eslint-disable prettier/prettier */
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Button,
+} from 'react-native';
+import {RadioButton} from 'react-native-paper';
+import {useNavigation} from '@react-navigation/native';
 
 const QuestionsScreen = () => {
   const navigation = useNavigation();
   const [answers, setAnswers] = useState({});
   const questions = [
     {
-      question: "Apakah Anda pernah mengalami kekerasan fisik?",
-      code: "K001"
+      question: 'Apakah Anda pernah mengalami  ciri - ciri seperti Memukul,menampar, mencekik, menendang, penjambakan, mendorong secara kasar, penginjakan, gigitan, cubitan, melempar barang ke tubuh korban, melukai dengan tangan kosong atau alat/senjata seperti rotan, ikat pinggang, kayu, besi, sundutan rokok, penyiksaan menggunakan benda tajam?',
+      code: 'C001',
     },
     {
-      question: "Apakah Anda pernah mengalami kekerasan psikologis?",
-      code: "K002"
+      question: 'Apakah Anda pernah mengalami ciri - ciri seperti Perbuatan dan ucapan yang mengakibatkan ketakutan, hilangnya rasa percaya diri, hilangnya kemampuan untuk bertindak, dan rasa tidak berdaya, seperti berteriak-teriak, mengancam, merendahkan, membentak, ucapan kata-kata kotor, menyalahkan, melabeli, atau juga mengkambinghitamkan, menyumpah serapah, melecehkan, menguntit, dan memata-matai serta tindak-tindakan lain yang menimbulkan rasa takut yang ditujukan kepada korban, merendahkan keyakinan dan kepercayaan korban, memaksa ?',
+      code: 'C002',
     },
     {
-      question: "Apakah Anda pernah mengalami kekerasan ekonomi?",
-      code: "K003"
+      question: 'Apakah Anda pernah mengalami ciri - ciri seperti Kelalaian memberikan kebutuhan hidup seperti makan, pakaian, perumahan, kesehatan, dan sebagainya. membatasi dan/ atau melarang untuk bekerja yang layak di dalam atau di luar rumah, sehingga korban di bawah kendati orang tersebut. mengambil barang korban, menahan atau tidak memberikan pemenuhan kebutuhan finansial dan sebagainya?',
+      code: 'C003',
     },
     {
-      question: "Apakah Anda pernah mengalami kekerasan seksual?",
-      code: "K004"
+      question: 'Apakah Anda pernah mengalami ciri - ciri seperti Perkosaan, intimidasi seksual termasuk ancaman atau percobaan perkosaan, pelecehan seksual, prostitusi paksa, perbudakan seksual, pemaksaan perkawinan, termasuk cerai gantung, pemaksaan kehamilan, pemaksaan aborsi, pemaksaan kontrasepsi dan sterilisasi, penyiksaan seksual, penghukuman tidak manusiawi dan bernuansa seksual, praktik tradisi bernuansa seksual yang membahayakan atau mendiskriminasi perempuan, Kontrol seksual, termasuk lewat aturan diskriminatif beralasan moralitas dan agama, menyentuh, meraba, mencium, memaksa korban melihat pornografi, ucapan yang merendahkan, melecehkan atau menyakiti korban. Seseorang laki-laki menaruh penis, jari atau benda apapun kedalam vagina, anus, atau mulut atau tubuh perempuan tanpa sekendak perempuan itu?',
+      code: 'C004',
     },
     {
-      question: "Apakah Anda pernah menjadi korban perdagangan orang (trafficking)?",
-      code: "K005"
+      question:
+        'Apakah Anda pernah mengalami ciri - ciri seperti Perekrutan, penampungan, pengiriman, pemindahan atau penerimaan seseorang dengan ancaman kekerasan, penggunaan kekerasan, penculikan, penyekapan, pemalsuan, penipuan, penyalahgunaan kekuasaan/ posisi rentan, penjeratan hutang/ member bayaran/ manfaat yang mengakibatkan orang tereksploitasi?',
+      code: 'C005',
     },
     {
-      question: "Apakah Anda pernah dieksploitasi?",
-      code: "K006"
+      question: 'Apakah Anda pernah mengalami ciri - ciri seperti Pelacuran, kerja atau pelayanan paksa, perbudakan, atau praktek serupa perbudakan, penindasan, pemerasan, pemanfaatan fisik, seksual, organ produksi, atau secara melawan hukum, memindahkan atau mentransplantasi organ dan/atau jaringan tubuh atau memanfaatkan tenaga atau kemampuan seseorang oleh pihak lain untuk mendapatkan keuntungan baik materil maupun immateril ?',
+      code: 'K006',
     },
   ];
 
   const handleOptionSelect = (code, value) => {
-    setAnswers({ ...answers, [code]: value });
+    setAnswers({...answers, [code]: value});
   };
 
   const isAllQuestionsAnswered = () => {
@@ -43,53 +52,62 @@ const QuestionsScreen = () => {
 
   const handleSubmitAnswers = () => {
     if (isAllQuestionsAnswered()) {
+      const trueKeys = Object.keys(answers).filter(key => answers[key]);
+      console.log(trueKeys);
       const result = forwardChaining(answers);
-      navigation.navigate('Result', { result });
+      const resultArray = Object.keys(result).filter(key => result[key]);
+      console.log(resultArray);
+      console.log(result);
+      navigation.navigate('Result', {result});
     } else {
-      alert("Silakan lengkapi semua pertanyaan sebelum menyelesaikan.");
+      alert('Silakan lengkapi semua pertanyaan sebelum menyelesaikan.');
     }
   };
 
-  const forwardChaining = (answeredValues) => {
-    let result = "Belum Diketahui"; // Hasil awal
-    console.log(answeredValues);
+  function forwardChaining(facts) {
+    let inferredFacts = {};
     const rules = [
-      {
-        condition: () => answeredValues["K001"], // K001: Kekerasan fisik
-        result: "Kekerasan fisik"
-      },
-      {
-        condition: () => answeredValues["K002"], // K002: Kekerasan psikologis
-        result: "Kekerasan psikologis"
-      },
-      {
-        condition: () => answeredValues["K003"], // K003: Kekerasan ekonomi
-        result: "Kekerasan ekonomi"
-      },
-      {
-        condition: () => answeredValues["K004"], // K004: Kekerasan seksual
-        result: "Kekerasan seksual"
-      },
-      {
-        condition: () => answeredValues["K005"], // K005: Perdagangan Orang (trafficking)
-        result: "Perdagangan Orang (trafficking)"
-      },
-      {
-        condition: () => answeredValues["K006"], // K006: Eksploitasi
-        result: "Eksploitasi"
-      },
-    ];
+      { conditions: ['C001'], result: ['K001'] },
+      { conditions: ['C002'], result: ['K002'] },
+      { conditions: ['C003'], result: ['K003'] },
+      { conditions: ['C004'], result: ['K004'] },
+      { conditions: ['C005'], result: ['K005'] },
+      { conditions: ['C006'], result: ['K006'] },
+      { conditions: ['C004'], result: ['K004'] },
+      { conditions: ['K001','K002','K003','K004','K005','K006'], result: ['P001'] },
+      { conditions: ['K001','K004'], result: ['P002'] },
+      { conditions: ['K001','K002','K003','K004','K005','K006'], result: ['P003'] },
+      { conditions: ['K004'], result: ['P004'] },
+      { conditions: ['K005','K006'], result: ['P005'] },
+      { conditions: ['K004','K006'], result: ['P007'] },
+      { conditions: ['K001','K002','K003','K004'], result: ['P008'] },
+      { conditions: ['K001','K002','K003','K004'], result: ['P009'] },
+      { conditions: ['K005','K006'], result: ['P010'] },
+      { conditions: ['K004','K006'], result: ['P011'] },
+  ];
+    while (true) {
+        let newInferences = false;
 
-    for (let i = 0; i < rules.length; i++) {
-      const rule = rules[i];
-      if (rule.condition()) {
-        result = rule.result;
-        break; 
-      }
+        for (let i = 0; i < rules.length; i++) {
+            const rule = rules[i];
+            const conditions = rule.conditions;
+            const result = rule.result;
+
+            if (conditions.every(condition => inferredFacts[condition] || facts[condition])) {
+                if (!inferredFacts[result]) {
+                    inferredFacts[result] = true;
+                    newInferences = true;
+                }
+            }
+        }
+
+        if (!newInferences) {
+            break;
+        }
     }
 
-    return result;
-  };
+    return inferredFacts;
+}
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -98,8 +116,7 @@ const QuestionsScreen = () => {
           <Text style={styles.questionText}>{question}</Text>
           <TouchableOpacity
             style={styles.optionContainer}
-            onPress={() => handleOptionSelect(code, true)}
-          >
+            onPress={() => handleOptionSelect(code, true)}>
             <RadioButton.Android
               value="true"
               status={answers[code] === true ? 'checked' : 'unchecked'}
@@ -109,8 +126,7 @@ const QuestionsScreen = () => {
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.optionContainer}
-            onPress={() => handleOptionSelect(code, false)}
-          >
+            onPress={() => handleOptionSelect(code, false)}>
             <RadioButton.Android
               value="false"
               status={answers[code] === false ? 'checked' : 'unchecked'}
@@ -141,6 +157,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginBottom: 10,
     color: 'blue',
+    textAlign:'justify',
   },
   optionContainer: {
     flexDirection: 'row',
