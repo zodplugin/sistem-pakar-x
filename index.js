c151cddafee7a997e04fb4cc47bf82c9ee6e5f51
zodplugin <zodplugin@gmail.com>
2024-07-21 18:11:34 +0700

    feat: add comment

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
-
+// App registry
 AppRegistry.registerComponent(appName, () => App);
